ac9842ef3bfefb685e99d58e7f66538a313fefca
Nandhini R P <125718973+nandhinirp71@users.noreply.github.com>
2025-08-07 05:26:40 +0530

    Update and rename bubblesort.py to sort.py

---
+++ python/sort.py
@@ -0,0 +1,32 @@
+def bubbleSort(arr):
+  # Time Complexity: O(n^2)
+  # Space Complexity: O(1)
+  n = len(arr)
+  flag = True
+  while flag:
+    flag = False
+    for i in range(1, n):
+      if  arr[i-1] > arr[i]:
+        arr[i-1], arr[i] = arr[i], arr[i-1]
+        flag = True
+
+  return arr
+
+def insertionSort(arr):
+  # Time Complexity: O(n^2)
+  # Space Complexity: O(1)
+  n = len(arr)
+  for i in (1, n):
+    for j in (1, 0, -1):
+      if arr[j} < arr[j-1]:
+        arr[j], arr[j-1] = arr[j-1], arr[j]
+      else:
+        break
+  return arr      
+
+a = [-5, 3, 2, 1, -3, -3, 7, 2, 2]
+bubbleSort(a)
+insertionSort(a)
+
+
+
