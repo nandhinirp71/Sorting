f32c820814f75f4fdc1dcca4115935997b4f32bd
Nandhini R P <125718973+nandhinirp71@users.noreply.github.com>
2025-08-07 05:41:05 +0530

    Update sort.py

--- python/sort.py
+++ python/sort.py
@@ -24,6 +24,19 @@ def insertionSort(arr):
         break
   return arr      
 
+def selectionSort(arr):
+  # Time Complexity: O(n^2)
+  # Space Complexity: O(1)
+  n = len(arr)
+  for i in range(1, n):
+    min_index = i
+    for j in range(i+1, n):
+      if arr[j] < arr[min_index]:
+        min_index = j
+    arr[i], arr[min_index] = arr[min_index], arr[i]
+  return arr
+      
+
 a = [-5, 3, 2, 1, -3, -3, 7, 2, 2]
 bubbleSort(a)
 insertionSort(a)
